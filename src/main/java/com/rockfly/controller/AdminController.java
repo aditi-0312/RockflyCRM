01f5db8d1fa5fa05c3cfc276f76cac92fc28fded
itanmay728 <ktanmay1130@gmail.com>
2024-05-01 18:20:01 +0530

    Dashboard UI changed, added addEmployee page ✔

--- src/main/java/com/rockfly/controller/AdminController.java
+++ src/main/java/com/rockfly/controller/AdminController.java
@@ -7,20 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+	
 	@GetMapping("/dashboard")
-	public String getDashboard() {
-		return "Admin/AdminDashboard";
+	public String getpage() {
+		return "pages/dashboard";
 	}
 	
 	@GetMapping("/addEmployee")
 	public String getAddEmployeePage() {
-		return "Admin/AddEmployee";
-	}
-	
-	@GetMapping("/test")
-	public String gettest() {
-		return "Admin/test";
+		return "pages/AddEmployee";
 	}
 	
 }
